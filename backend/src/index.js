ee3a1d18393edf383c6ed018435ce5824636a29e
jaiswaldeepali27 <jaiswaldeepali110@gmail.com>
2025-04-30 14:54:44 +0530

    debug

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(cors({
 }));
 
 // app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+// app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
