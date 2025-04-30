d30f5fc77b7218f91b895f2e1bfd6e34afaca21e
jaiswaldeepali27 <jaiswaldeepali110@gmail.com>
2025-04-30 15:16:00 +0530

    debug

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,8 +1,8 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 import cloudinary from "../lib/cloudinary.js";
-import { io } from "../lib/socket.js";
-import { getReceiverSocketId } from "../lib/socket.js";
+import { io , getReceiverSocketId } from "../lib/socket.js";
+
 
 export const getUserForSidebar = async (req, res) => {
     try {
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,9 +1,41 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRoutes from "../routes/auth.route.js";
+import messageRoutes from "../routes/message.route.js";
+import { connectDB } from "./db.js";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+
+
+// CORS + Middleware
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Serve frontend in production
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 const io = new Server(server, {
     cors: {
@@ -11,20 +43,20 @@ const io = new Server(server, {
     }
 });
 
+const userSocketMap = {};
+
 export function getReceiverSocketId(userId) {
     return userSocketMap[userId] || null;
 }
 
-const userSocketMap = {} 
-
 io.on("connection", (socket) => {
     console.log("A user Connected", socket.id);
     const userId = socket.handshake.query.userId;
-    if(userId) {
+    if (userId) {
         userSocketMap[userId] = socket.id;
     }
 
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
         console.log("A user Disconnected", socket.id);
@@ -32,4 +64,7 @@ io.on("connection", (socket) => {
         io.emit("getOnlineUsers", Object.keys(userSocketMap));
     });
 });
-export {io, app, server};
+
+connectDB();
+
+export { io, app, server };
