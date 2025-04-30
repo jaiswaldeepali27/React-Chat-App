450f6935de87ffa2f9adc61a0fb95e50cbf3d699
jaiswaldeepali27 <jaiswaldeepali110@gmail.com>
2025-04-30 14:52:59 +0530

    debug

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use("/api/auth", authRoutes);
+// app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV==="production"){
